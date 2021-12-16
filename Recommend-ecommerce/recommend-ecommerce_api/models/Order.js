00bf30bbf1ec291e5b5b00116c3d3dba9b9bea80
Ha Phong <haphong702@gmail.com>
2021-12-16 22:47:02 +0700

    Haphong

---
+++ Recommend-ecommerce/recommend-ecommerce_api/models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true},
+    usersname:{type: String, required: false,unique:false},
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: false },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
