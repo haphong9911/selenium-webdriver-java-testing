66124a499d7f589eb8b2d727d5b5dc5c449e796f
Ha Phong <haphong702@gmail.com>
2022-09-25 21:26:25 +0700

    Add Topic 02

---
+++ Automation/Project/selenium/javaTester/Topic_01.java
@@ -0,0 +1,11 @@
+package javaTester;
+
+public class Topic_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Automation tester";
+		System.out.println(name);
+	}
+
+}
