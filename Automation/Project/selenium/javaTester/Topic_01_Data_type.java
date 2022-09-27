05d18e83e064c3058c668b65231af20feaca0e61
Ha Phong <haphong702@gmail.com>
2022-09-27 20:35:53 +0700

    a

--- Automation/Project/selenium/javaTester/Topic_01_Data_type.java
+++ Automation/Project/selenium/javaTester/Topic_01_Data_type.java
@@ -1,5 +1,11 @@
 package javaTester;
 
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Topic_01_Data_type {
 
 	public static void main(String[] args) {
@@ -23,11 +29,31 @@ public class Topic_01_Data_type {
 		//long
 		long lEmployee = 10000;
 		//float
-		float fEmployee= 10	;
+		float fEmployee= 10f	;
 		//double
+		double dEmployee = 8.4d;
 		//char
+		char a = 'B';
 		
 		//II - Tham chiếu (Reference)
+		//Array
+		int[] studenNumbers = {15,50,-7};
+		String[] studenName = {"aass","as"};
+		
+		//Class / Interface
+		WebDriver driver = new ChromeDriver();
+		
+		Actions action = new Actions(driver);
+		
+		//Collection: List/Set/Queue
+		ArrayList<String> studenCountry = new ArrayList<String>();
+		
+		//Object
+		Object phone;
+		
+		//Chuỗi kí tự - String
+		String studentName = "B";
+		String companyName = "Tran Ha Phong";
 	}
 
 }
